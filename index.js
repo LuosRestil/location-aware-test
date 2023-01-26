2c1725901e3bafe5c17d7b197bc9584be0b98350
Luos Restil <luosrestil@Luoss-MacBook-Pro.local>
2023-01-26 17:35:43 -0600

    more test stuff...

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 function main() {
   window.addEventListener('deviceorientation', event => {
     handleOrientationChange(event);
+  });
+
+  window.addEventListener('devicemotion', event => {
+    handleMotion(event);
   })
 }
 
 function handleOrientationChange(event) {
-  document.getElementById('device-moved').textContent = 'TRUE';
+  document.getElementById('device-moved').textContent = 'device orientation';
   document.getElementById('display').textContent = `${event.alpha}, ${event.beta}, ${event.gamma}`;
+}
+
+function handleMotion(event) {
+  document.getElementById('device-moved').textContent = 'device motion'
 }
