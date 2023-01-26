e7f9f2865eddba21ae275761bdb1eb7a94a483f2
Luos Restil <luosrestil@Luoss-MacBook-Pro.local>
2023-01-26 17:33:51 -0600

    add test for firing of handleOrientationChange

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 function main() {
   window.addEventListener('deviceorientation', event => {
     handleOrientationChange(event);
-    // 
   })
 }
 
 function handleOrientationChange(event) {
+  document.getElementById('device-moved').textContent = 'TRUE';
   document.getElementById('display').textContent = `${event.alpha}, ${event.beta}, ${event.gamma}`;
 }
