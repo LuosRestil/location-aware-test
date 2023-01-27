3ec330c2f3b300ab3befbd154e9537ae7d5dc0db
Luos Restil <luosrestil@Luoss-MacBook-Pro.local>
2023-01-27 09:35:01 -0600

    add velocity calcs

--- index.js
+++ index.js
@@ -4,13 +4,19 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const ctx = canvas.getContext("2d");
 ctx.font = '20px sans-serif';
-let beta;
-let gamma;
+let beta = 0;
+let gamma = 0;
 const radius = 20;
 const centerX = Math.round(canvas.width / 2);
 const centerY = Math.round(canvas.height / 2);
 let circleX = centerX;
 let circleY = centerY;
+let vx = 0;
+let vy = 0;
+const accelRate = 0.01;
+maxVelocity = 2;
+// let ax = 0;
+// let ay = 0;
 
 
 startButton.addEventListener('click', () => {
@@ -27,15 +33,31 @@ window.addEventListener('deviceorientation', event => {
 });
 
 function handleOrientationChange(event) {
-  beta = Math.round(event.beta);
-  gamma = Math.round(event.gamma);
+  beta = event.beta;
+  gamma = event.gamma;
 }
 
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  circleX = centerX + (gamma * 2);
-  circleY = centerY + (beta * 4);
+  moveCircle()
+  drawCircle()
+
+  ctx.fillText('gamma: ' + Math.round(gamma), 10, 20);
+  ctx.fillText('beta: ' + Math.round(beta), 10, 50);
+
+  window.requestAnimationFrame(draw)
+}
+
+function moveCircle() {
+  vx += Math.min(gamma * accelRate, maxVelocity);
+  vy += Math.min(beta * accelRate, maxVelocity);
+  circleX += vx;
+  circleY += vy;
+
+  // circleX = centerX + (gamma * 2);
+  // circleY = centerY + (beta * 4);
+
 
   if (circleX > canvas.width - radius) {
     circleX = canvas.width - radius;
@@ -48,7 +70,9 @@ function draw() {
   } else if (circleY < radius) {
     circleY = radius;
   }
+}
 
+function drawCircle() {
   ctx.beginPath();
   ctx.arc(circleX, circleY, radius, 0, 2 * Math.PI, false);
   ctx.fillStyle = 'black';
@@ -56,9 +80,5 @@ function draw() {
   ctx.lineWidth = 5;
   ctx.strokeStyle = '#FF0000';
   ctx.stroke();
-
-  ctx.fillText('gamma: ' + gamma, 10, 20);
-  ctx.fillText('beta: ' + beta, 10, 50);
-
-  window.requestAnimationFrame(draw)
 }
+
